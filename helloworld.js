5b1ff992c4f95cff842f7c45b91fb210200d068e
dasamaf <haltriescode@gmail.com>
2024-11-30 23:45:42 +0700

    learn javascript

---
+++ helloworld.js
@@ -0,0 +1,15 @@
+console.log("Hello World!");
+
+//creating variable with let vs const
+
+let a = 10;
+a = 15;
+console.log( a ); // output 15
+
+const b = 20;
+b = 10;
+
+console.log (b); // output error
+
+
+
