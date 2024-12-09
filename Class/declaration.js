eae402eaa894287570e8cd97f390661d3ca99bd0
dasamaf <haltriescode@gmail.com>
2024-12-09 20:41:57 +0700

    learn JS Class

---
+++ Class/declaration.js
@@ -0,0 +1,20 @@
+//class declaration
+
+class Coin {
+    constructor(name, ticker){
+        this.name =name;
+        this.ticker = ticker;
+    }
+    buy(){
+        console.log(`Buy $${this.ticker}`);
+    }
+}
+
+// Creating instance from Coin
+
+const coin1 = new Coin('Bitcoin','BTC');
+const coin2 = new Coin('Bitcoin','ETH');
+
+console.log(coin1.name); // Output : Bitcoin
+
+coin1.buy(); // Output : Buy $BTC 
---
+++ Class/inheritance.js
@@ -0,0 +1,37 @@
+// Parent Class
+class Cryptocurrency {
+    constructor(name, ticker, marketCap){
+        this.name = name;
+        this.ticker = ticker;
+        this.marketCap = marketCap;
+    }
+
+    getInfo(){
+        return `${this.name} (${this.ticker}) has a market cap of $${this.marketCap} bilion.`;
+    }
+
+    transfer(amount, recipient) {
+        return `Transferred ${amount} ${this.ticker} to ${recipient}.`;
+    }
+};
+
+//Child Class
+class Altcoin extends Cryptocurrency {
+    constructor(name, ticker, marketCap, project) {
+        super(name, ticker, marketCap);
+        this.project = project; 
+    }
+
+    getPurpose() {
+        return `${this.name} ($${this.ticker}) is a ${this.project} coin.`;
+    }
+};
+
+
+//Example
+const doge = new Altcoin('Dogecoin', 'DOGE', 65, 'meme'); //DOGE marketcap currently around $65 bilion.
+console.log(doge.getInfo()); 
+console.log(doge.getPurpose()); 
+console.log(doge.transfer(1000, 'Bob')); 
+
+
